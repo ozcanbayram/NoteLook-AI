fc4147c61a97fdc1c5b819bc7c6d3452085c65cd
Ozcan Bayram <ozzcanbayram@gmail.com>
2024-05-21 19:04:17 +0300

    deleted a line because my project is not using this line

--- lib/screens/add_note_screen.dart
+++ lib/screens/add_note_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:note_ai/models/note.dart';
 
 class AddNoteScreen extends StatelessWidget {
   final TextEditingController _titleController = TextEditingController();
